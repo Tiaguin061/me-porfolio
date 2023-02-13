50ec412535301bd95c32abb64c2f1691cd5e5186
Tiaguin061 <tiaguin180@gmail.com>
2023-02-12 22:49:41 -0300

    feat: Added react carrousel lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { theme } from "@/styles/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
