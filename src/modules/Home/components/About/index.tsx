5bbce17db6774dbf4f752bb9293bbfa1927e33e6
Tiaguin061 <tiaguin180@gmail.com>
2023-03-04 09:35:50 -0300

    refactor: Changed texts on about me

--- src/modules/Home/components/About/index.tsx
+++ src/modules/Home/components/About/index.tsx
@@ -18,23 +18,23 @@ export function About() {
 
             <Flex flexDirection="column" mt={4}>
               <Text as="p" color="gray.300" textIndent="20px">
-                Em Janeiro de 2020, comecei a conhecer o mundo da programação e,
-                ao longo dos meses, fui me aprofundando até perceber que essa
-                área é incrível e que quero seguir essa carreira.
+                Em Janeiro de 2020, entrei na área de programação e, ao longo
+                dos meses, fui me aprofundando até perceber que essa área é
+                incrível e defini seguir carreira.
               </Text>
               <Text as="p" color="gray.300" textIndent="20px" mt={1.5}>
-                Nesse mesmo ano, comecei a trabalhar como freelancer em ReactJS,
-                o que me deu a oportunidade de ingressar na carreira
-                profissional. Desde então, desenvolvi alguns projetos como
-                freelancer, mas ainda não trabalhei de forma fixa em uma
-                empresa. Isso me motiva a estudar mais e buscar ser um
-                profissional cada vez melhor.
+                Nesse mesmo ano, comecei a trabalhar como freelancer, o que me
+                deu a oportunidade de ingressar na carreira profissional. Desde
+                então, desenvolvi alguns projetos como freelancer, mas ainda não
+                trabalhei de forma fixa em uma empresa. Isso me motiva a estudar
+                mais e buscar ser um profissional cada vez melhor.
               </Text>
               <Text as="p" color="gray.300" textIndent="20px" mt={1.5}>
-                Com o passar do tempo, sigo ganhando experiência e tendo como
-                principais características ser proativo, produtivo e
-                participativo. Além disso, tenho muito senso de trabalho em
-                equipe e sou capaz de encontrar soluções para problemas na área.
+                Gosto de me envolver em desafios, conversar com especialistas e
+                buscar sempre evoluir, encontrando soluções que agreguem o
+                melhor resultado para o cliente final. Tenho como principais
+                características a proatividade, produtividade, participação
+                ativa e senso de trabalho em equipe.
               </Text>
             </Flex>
           </Flex>
